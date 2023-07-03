98bcbd5a31b114d51074a1d4a53177da80e8ab74
PJ Fanning <pjfanning@users.noreply.github.com>
2023-07-03 16:31:59 +0100

    add disclaimer to jar

---
+++ project/AddMetaInfLicenseFiles.scala
@@ -0,0 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, which was derived from Akka.
+ */
+
+import org.mdedetrich.apache.sonatype.ApacheSonatypePlugin
+import org.mdedetrich.apache.sonatype.ApacheSonatypePlugin.autoImport.*
+import sbt.*
+import sbt.Keys.*
+
+/**
+ * Copies LICENSE and NOTICE files into jar META-INF dir
+ */
+object AddMetaInfLicenseFiles extends AutoPlugin {
+
+  private lazy val baseDir = LocalRootProject / baseDirectory
+
+  override lazy val projectSettings = Seq(
+    apacheSonatypeDisclaimerFile := Some(baseDir.value / "DISCLAIMER"))
+
+  override def trigger = allRequirements
+
+  override def requires = ApacheSonatypePlugin
+}
